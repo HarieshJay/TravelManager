3eb3da3b1ce36a32094a7e21e8ee282053a23f3a
Hariesh <jayanthh@mcmaster.ca>
2019-03-21 23:01:14 -0400

    Finished Navbar

---
+++ Pages/Dashboard/dashboard/src/aboutus.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const AboutUs = function() {
+  return (
+    <div className="d-block m-5">
+      <h1 className="d-block m-5">This is us</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
--- Pages/Dashboard/dashboard/src/sidenav.js
+++ Pages/Dashboard/dashboard/src/sidenav.js
@@ -1,205 +1,96 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./Styles.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import AboutUs from "./aboutus";
+
+// Side-nav tutorial https://bootstrapious.com/p/bootstrap-sidebar
 
 class Nav extends React.Component {
   render() {
     return (
-      <div>
-        <div className="wrapper">
-          <nav id="sidebar">
-            <div className="sidebar-header">
-              <h3>Bootstrap Sidebar</h3>
-            </div>
+      <Router>
+        <div>
+          <div className="wrapper">
+            <nav id="sidebar">
+              <div className="sidebar-header">
+                <h3>It Takes Two to Tango</h3>
+              </div>
 
-            <ul className="list-unstyled components">
-              <p>Dummy Heading</p>
-              <li className="active">
-                <a
-                  href="#homeSubmenu"
-                  data-toggle="collapse"
-                  aria-expanded="false"
-                  className="dropdown-toggle"
-                >
-                  Home
-                </a>
-                <ul className="collapse list-unstyled" id="homeSubmenu">
-                  <li>
-                    <a href="#">Home 1</a>
-                  </li>
-                  <li>
-                    <a href="#">Home 2</a>
-                  </li>
-                  <li>
-                    <a href="#">Home 3</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">About</a>
-                <a
-                  href="#pageSubmenu"
-                  data-toggle="collapse"
-                  aria-expanded="false"
-                  className="dropdown-toggle"
-                >
-                  Pages
-                </a>
-                <ul className="collapse list-unstyled" id="pageSubmenu">
-                  <li>
-                    <a href="#">Page 1</a>
-                  </li>
-                  <li>
-                    <a href="#">Page 2</a>
-                  </li>
-                  <li>
-                    <a href="#">Page 3</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">Portfolio</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-
-            <ul className="list-unstyled CTAs">
-              <li>
-                <a
-                  href="https://bootstrapious.com/tutorial/files/sidebar.zip"
-                  className="download"
-                >
-                  Download source
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://bootstrapious.com/p/bootstrap-sidebar"
-                  className="article"
-                >
-                  Back to article
-                </a>
-              </li>
-            </ul>
-          </nav>
-
-          <div id="content">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <div className="container-fluid">
-                <button
-                  type="button"
-                  id="sidebarCollapse"
-                  className="navbar-btn"
-                >
-                  <span />
-                  <span />
-                  <span />
-                </button>
-                <button
-                  className="btn btn-dark d-inline-block d-lg-none ml-auto"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <i className="fas fa-align-justify" />
-                </button>
-
-                <div
-                  className="collapse navbar-collapse"
-                  id="navbarSupportedContent"
-                >
-                  <ul className="nav navbar-nav ml-auto">
-                    <li className="nav-item active">
-                      <a className="nav-link" href="#">
-                        Page
-                      </a>
+              <ul className="list-unstyled components">
+                <p>Music is better with friends</p>
+                <li>
+                  <Link to="/AboutUs">About</Link>
+                </li>
+                <li>
+                  <a
+                    href="#pageSubmenu"
+                    data-toggle="collapse"
+                    aria-expanded="false"
+                    className="dropdown-toggle"
+                  >
+                    Pages
+                  </a>
+                  <ul className="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                      <a href="#">Page 1</a>
                     </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        Page
-                      </a>
+                    <li>
+                      <a href="#">Page 2</a>
                     </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        Page
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        Page
-                      </a>
+                    <li>
+                      <a href="#">Page 3</a>
                     </li>
                   </ul>
-                </div>
-              </div>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
             </nav>
 
-            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div id="content" className="d-block">
+              <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+                <div className="container-fluid">
+                  <button
+                    type="button"
+                    id="sidebarCollapse"
+                    className="navbar-btn"
+                  >
+                    <span />
+                    <span />
+                    <span />
+                  </button>
+                  <button
+                    className="btn btn-dark d-inline-block d-lg-none ml-auto"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <i className="fas fa-align-justify" />
+                  </button>
 
-            <div className="line" />
-
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-
-            <div className="line" />
-
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-
-            <div className="line" />
-
-            <h3>Lorem Ipsum Dolor</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul className="nav navbar-nav ml-auto">
+                      <li className="nav-item active">
+                        <a className="nav-link" href="#">
+                          Log out
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+              <Route exact path="/AboutUs" component={AboutUs} />
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
